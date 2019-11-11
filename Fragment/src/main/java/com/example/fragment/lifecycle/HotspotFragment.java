f9a2c74734c5fefc3fe126451f691b4a8ba62fe6
bunny <chendongming@rsdalstar-rd.com>
2019-11-11 15:09:50 +0800

    ...

--- Fragment/src/main/java/com/example/fragment/lifecycle/HotspotFragment.java
+++ Fragment/src/main/java/com/example/fragment/lifecycle/HotspotFragment.java
@@ -5,11 +5,11 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.baselibrary.utils.log.AppLogger;
 import com.example.fragment.R;
 
 /**
@@ -23,20 +23,20 @@ public class HotspotFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         //表示当activity和fragment产生关联时回调的方法
-        Log.d("tag", "------HotspotFragment------onAttach: Activity");
+        AppLogger.d("------HotspotFragment------onAttach: Activity");
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        Log.d("tag", "------HotspotFragment------onAttach: Context");
+        AppLogger.d("------HotspotFragment------onAttach: Context");
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //表示当fragment第一次被创建时回调的方法
-        Log.d("tag", "------HotspotFragment------onCreate:");
+        AppLogger.d("------HotspotFragment------onCreate:");
     }
 
 
@@ -46,7 +46,7 @@ public class HotspotFragment extends Fragment {
             , @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_hotspot, null);
         //表示当fragment第一次绘制用户界面时回调的方法
-        Log.d("tag", "------HotspotFragment------onCreateView:");
+        AppLogger.d("------HotspotFragment------onCreateView:");
         return view;
     }
 
@@ -54,55 +54,61 @@ public class HotspotFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //表示当fragment所属的activity创建成功时回调的方法
-        Log.d("tag", "------HotspotFragment------onActivityCreated:");
+        AppLogger.d("------HotspotFragment------onActivityCreated:");
     }
 
     @Override
     public void onStart() {
         super.onStart();
         //表示当fragment能够被用户看到时回调的方法
-        Log.d("tag", "------HotspotFragment------onStart:");
+        AppLogger.d("------HotspotFragment------onStart:");
     }
 
     @Override
     public void onResume() {
         super.onResume();
         //表示当fragment能够获取用户焦点时回调的方法
-        Log.d("tag", "------HotspotFragment------onResume:");
+        AppLogger.d("------HotspotFragment------onResume:");
     }
 
     @Override
     public void onPause() {
         super.onPause();
         //表示当fragment失去用户焦点时回调的方法
-        Log.d("tag", "------HotspotFragment------onPause:");
+        AppLogger.d("------HotspotFragment------onPause:");
     }
 
     @Override
     public void onStop() {
         super.onStop();
         //表示当fragment完全被用户遮挡时回调的方法
-        Log.d("tag", "------HotspotFragment------onStop:");
+        AppLogger.d("------HotspotFragment------onStop:");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         //表示fragment视图在activity中移除时回调的方法
-        Log.d("tag", "------HotspotFragment------onDestroyView:");
+        AppLogger.d("------HotspotFragment------onDestroyView:");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         //表示当fragment被销毁时回调的方法
-        Log.d("tag", "------HotspotFragment------onDestroy:");
+        AppLogger.d("------HotspotFragment------onDestroy:");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         //表示当fragment与activity失去关联时回调的方法
-        Log.d("tag", "------HotspotFragment------onDetach:");
+        AppLogger.d("------HotspotFragment------onDetach:");
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        AppLogger.d("------HotspotFragment------onHiddenChanged: hidden=" + hidden);
     }
 }
--- Fragment/src/main/java/com/example/fragment/lifecycle/LifeCycleActivity.java
+++ Fragment/src/main/java/com/example/fragment/lifecycle/LifeCycleActivity.java
@@ -1,14 +1,14 @@
 package com.example.fragment.lifecycle;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.baselibrary.utils.log.AppLogger;
 import com.example.fragment.R;
 
 /**
@@ -17,7 +17,7 @@ import com.example.fragment.R;
  * 演示Fragment的生命周期
  */
 
-public class LifeCycleActivity extends Activity implements View.OnClickListener {
+public class LifeCycleActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_hotspot, tv_topline;
     private FragmentManager manager;
@@ -43,7 +43,7 @@ public class LifeCycleActivity extends Activity implements View.OnClickListener 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle);
-        Log.d("tag", "------LifeCycleActivity------onCreate: ");
+        AppLogger.d("------LifeCycleActivity------onCreate: ");
 
         tv_hotspot = (TextView) findViewById(R.id.hotspot);
         tv_topline = (TextView) findViewById(R.id.topline);
@@ -58,46 +58,46 @@ public class LifeCycleActivity extends Activity implements View.OnClickListener 
     }
 
     @Override
+    protected void onRestart() {
+        super.onRestart();
+        //如果现在处于停止状态，重新被用户使用
+        AppLogger.d("------LifeCycleActivity------onRestart: ");
+    }
+
+
+    @Override
     protected void onStart() {
         super.onStart();
         //用户被看到
-        Log.d("tag", "------LifeCycleActivity------onStart: ");
+        AppLogger.d("------LifeCycleActivity------onStart: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //获取用户焦点
-        Log.d("tag", "------LifeCycleActivity------onResume: ");
+        AppLogger.d("------LifeCycleActivity------onResume: ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         //失去用户焦点
-        Log.d("tag", "------LifeCycleActivity------onPause: ");
+        AppLogger.d("------LifeCycleActivity------onPause: ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         //完全被其他界面遮挡时
-        Log.d("tag", "------LifeCycleActivity------onStop: ");
+        AppLogger.d("------LifeCycleActivity------onStop: ");
     }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        //如果现在处于停止状态，重新被用户使用
-        Log.d("tag", "------LifeCycleActivity------onRestart: ");
-    }
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //如果直接点击返回
-        Log.d("tag", "------LifeCycleActivity------onDestroy: ");
+        AppLogger.d("------LifeCycleActivity------onDestroy: ");
     }
 
 }
