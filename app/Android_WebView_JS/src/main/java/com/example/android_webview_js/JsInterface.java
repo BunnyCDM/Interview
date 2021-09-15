a8dac67518f771f07488e608eedcd44599363126
chendongming <chendongming@unwulian.com>
2021-09-15 11:05:08 +0800

    js验证调试

--- app/Android_WebView_JS/src/main/java/com/example/android_webview_js/JsInterface.java
+++ app/Android_WebView_JS/src/main/java/com/example/android_webview_js/JsInterface.java
@@ -1,29 +1,29 @@
-//package com.example.android_webview_js;
-//
-//import android.webkit.JavascriptInterface;
-//
-//import com.example.baselibrary.utils.log.AppLogger;
-//
-///**
-// * Created by mac on 2020-04-20.
-// */
-//public class JsInterface {
-//
-//    private JsBridge jsBridge;
-//
-//    public JsInterface(JsBridge jsBridge) {
-//        this.jsBridge = jsBridge;
-//    }
-//
-//    /**
-//     * 这个不在主线长执行
-//     *
-//     * @param value
-//     */
-//    @JavascriptInterface //让webview识别
-//    public void setValue(String value) {
-//        AppLogger.d("setValue: value=" + value);
-//        jsBridge.setTextViewValue(value);
-//    }
-//
-//}
+package com.example.android_webview_js;
+
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+
+
+/**
+ * Created by mac on 2020-04-20.
+ */
+public class JsInterface {
+
+    private JsBridge jsBridge;
+
+    public JsInterface(JsBridge jsBridge) {
+        this.jsBridge = jsBridge;
+    }
+
+    /**
+     * 这个不在主线长执行
+     *
+     * @param value
+     */
+    @JavascriptInterface //让webview识别
+    public void setValue(String value) {
+        Log.d("bunny", "setValue: value=" + value);
+        jsBridge.setTextViewValue(value);
+    }
+
+}
---
+++ app/Retrofit/src/main/java/com/asw/retrofit/Constant.java
@@ -0,0 +1,31 @@
+package com.asw.retrofit;
+
+/**
+ * Created by mac on 2017/4/14.
+ */
+
+public class Constant {
+    //start
+    // http://v.juhe.cn/weather/index/cityname=%E5%90%88%E8%82%A5&dtype=&format=&key=f932e05d3673dd26f518fc9d10e92ca3
+    public final static String WEATHER_URL="http://v.juhe.cn/weather/index";
+    //end
+
+    //荔枝新闻的完成的url地址
+    public final static String URL = "http://litchiapi.jstv.com/" +
+            "api/GetFeeds?column=0&PageSize=20&pageIndex=1&val=100511D3BE5301280E0992C73A9DEC41";
+
+//    public final static String BASE_URL = "http://litchiapi.jstv.com/";
+    public final static String BASE_URL = "http://news.ahwang.cn/hefei/";//合肥新闻网 http://news.ahwang.cn/hefei/
+//    public final static String TEABASE_URL = "http://42.120.4.67/api/app/business/";
+//    public final static String TEABASE_URL = "http://www.xhlj.org/";//http://www.xhlj.org/产品名称-12
+    public final static String TEABASE_URL = "http://www.tea.com/";//http://www.xhlj.org/产品名称-12
+
+    //下载图片地址
+    public final static String IMAGE_URL = "http://img5.imgtn.bdimg.com/it/u=4080105893,4096129906&fm=206&gp=0.jpg/";
+
+    //登录地址  http://10.0.152.244:8080/WebServiceDemo/LoginServlet
+    public final static String LOGIN_URL = "http://10.0.165.133:8080/WebServiceDemo/";
+
+    //上传的网络地址   http://192.168.1.103:8080/UploadServer/UpLoadAction
+    public final static String UPLOAD_URL = "http://10.0.165.133:8080/UploadServer/";
+}
