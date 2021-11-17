ce84046caa9a8d0a7283c8871b137f1ad8e3c244
chendongming <chendongming@unwulian.com>
2021-11-17 14:16:08 +0800

    ...

--- app/JNI/.cxx/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ app/JNI/.cxx/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/mac/work/Android/Workspace/Studio/Interview/app/JNI
+# Install script for directory: /Users/mac/work/Android/workspace/Studio/Interview/app/JNI
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/mac/work/Android/Workspace/Studio/Interview/app/JNI/.cxx/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/mac/work/Android/workspace/Studio/Interview/app/JNI/.cxx/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
